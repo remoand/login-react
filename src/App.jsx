37f20cde1c815493a0d635026e920e6a8b437eb7
R3S214 <s2370449@s.do-johodai.ac.jp>
2026-01-29 16:03:51 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Top from "./pages/Top";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import LoginSuccess from "./pages/Login_Success";
 function App() {
  return (
  <Routes>
  <Route path="/" element={<Top />} />
+ <Route path="/login" element={<Login />} />
+ <Route path="/signup" element={<Signup />} />
+ <Route path="/success" element={<LoginSuccess />} />
  </Routes>
  );
 }
